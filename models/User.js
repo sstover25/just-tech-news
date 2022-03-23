c0310b921e923a5693695ef85846db29e559ee84
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-23 15:15:53 -0500

    added password encryption when user changes password in PUT request

--- models/User.js
+++ models/User.js
@@ -53,6 +53,14 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
     },
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
     // pass in our imported sequelize connection (the direct connection to our database)
