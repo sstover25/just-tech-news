8c6e5bc9da592e775e78d2a4b90d6152f4eac176
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-26 16:14:56 -0500

    adding conditional rendering

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,7 +35,7 @@ router.get("/", (req, res) => {
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
       // pass a single post object into the homepage template
-      res.render("homepage", { posts });
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
     })
     .catch((err) => {
       console.log(err);
@@ -94,7 +94,10 @@ router.get("/post/:id", (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render("single-post", { post });
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
